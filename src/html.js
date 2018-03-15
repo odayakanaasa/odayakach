4d2249380bd59f8a1ab851184455bf11364fc142
odayaka <odayakanaasa@gmail.com>
2018-03-15 14:20:25 +0700

    update CA PUB ID

--- src/html.js
+++ src/html.js
@@ -68,7 +68,7 @@ export default class HTML extends Component {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-4162878936142053',
+    google_ad_client: 'ca-pub-IDHERE',
     enable_page_level_ads: true
   });
   `;
